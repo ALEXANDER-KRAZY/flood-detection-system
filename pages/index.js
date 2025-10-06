075391f335f6632a4f2e5ddb689cc0bf2bf96657
Alex Mutuku <81432059+ALEXANDER-KRAZY@users.noreply.github.com>
2025-10-06 17:49:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { useRouter } from "next/router"
-import BlueButton from "../components/BlueButton";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("")
@@ -29,9 +28,7 @@ export default function LoginPage() {
         <h1 className="text-3xl font-bold text-center color-blue-800">
           Flood Detection System Login
         </h1>
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-      <BlueButton />
-    </div>
+  
         {error && (
           <div className="bg-red-100 text-red-700 px-4 py-2 rounded-md mb-4 text-sm text-center">
             {error}
