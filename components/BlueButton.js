06a3284b22cc42bb21a912c78bc4971aae761086
Alex Mutuku <81432059+ALEXANDER-KRAZY@users.noreply.github.com>
2025-10-06 17:48:27 +0300

    Update BlueButton.js

--- components/BlueButton.js
+++ components/BlueButton.js
@@ -1,12 +1,1 @@
-// components/BlueButton.js
-import React from "react";
 
-export default function BlueButton() {
-  return (
-    <button
-      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg shadow-md transition-transform transform hover:scale-105"
-    >
-      Hi
-    </button>
-  );
-}
